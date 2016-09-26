3d6ab300f2b0b6ac79fe6c7a331c621905785789
chenfengxiang <1154165844@qq.com>
2016-09-26 14:22:31 +0800

    first commit

---
+++ src/com/cfx/test/Test.java
@@ -0,0 +1,16 @@
+package com.cfx.test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServlet;
+
+public class Test extends HttpServlet{
+
+	public void money(){
+		Map< String, String> map = new HashMap<String, String>();
+		map.put("1", "未结算");
+		map.put("2", "结算中");
+		map.put("3", "结算中");
+	}
+}
